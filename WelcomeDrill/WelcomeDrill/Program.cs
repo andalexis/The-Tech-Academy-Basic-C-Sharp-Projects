14cbd89ca48032b1c47f97f245507421a39791c7
Alexis <a.anderson4110@gmail.com>
2019-07-27 17:15:24 -0500

    WelcomeDrill
    Runs a welcome statement

---
+++ WelcomeDrill/WelcomeDrill/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accoutning Systems" + "\r\n" + "Remember we're 'accounting' on you!");
+        Console.ReadLine();
+    }
+}
+

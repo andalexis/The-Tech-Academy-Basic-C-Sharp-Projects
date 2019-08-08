b7cb937e07c573f8d276178f86c98f887fbc93be
Alexis <a.anderson4110@gmail.com>
2019-08-08 15:46:42 -0500

    boolean Logic
    practice drills

---
+++ booleanLogic/booleanLogic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace booleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(true && false); // is false since it asks if both are true
+            //Console.WriteLine(true || true); // true or true
+
+            //Console.WriteLine(true == true); //does true equal true
+            //Console.WriteLine(true == false); //does true equal false
+            //Console.WriteLine(true != true); // says does true does not equal true? result false
+
+            Console.WriteLine(true ^ true); //operator evaulates to true
+            Console.WriteLine(true ^ false); //if ones of these are true but not both
+            Console.WriteLine(false ^ false); // xor operator
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ insuranceApproval/insuranceApproval/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace insuranceApproval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine(age);
+            Console.WriteLine("Have you ever had a DUI?: ");
+            bool duiCheck = Convert.ToBoolean(Console.ReadLine());
+            // Console.WriteLine(duiCheck);
+            Console.WriteLine("How many speeding tickets do you have?: ");
+            int speedTicket = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine(speedTicket);
+
+            Console.WriteLine("Qualified?");
+            bool isQualified = (age > 15 && duiCheck == false && speedTicket < 3);
+            Console.WriteLine(isQualified);
+            Console.ReadLine();
+        }
+    }
+}

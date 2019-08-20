366620e8781442277e43324964acc5558d77818b
Alexis <a.anderson4110@gmail.com>
2019-08-20 18:32:58 -0500

    strings

---
+++ stringDrill/stringDrill/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string line1 = "The quick brown";
+            string line2 = "fox jumped over";
+            string line3 = "the lazy dog.";
+
+            string text = line1 + " " + line2 + " " + line3;
+            Console.WriteLine(text.ToUpper());
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("I think we can all agree that movie theater snacks are overpriced.");
+            sb.Append(" My friends and I wanted to go see a movie and we decided to smuggle in food and drinks.");
+            sb.Append(" I wanted a sprite so I poured it into a water bottle.");
+            sb.AppendLine(" My mistake was using a water bottle with one of those flip up straws...");
+            sb.Append(" At the theater, a few minutes into the start of the movie, I wanted my sprite.");
+            sb.Append(" I flipped up the rubber straw and a volcano of fizzy sprite spewed 6 FEET in the air, soaking everyone around me!");
+            sb.AppendLine(" I ran out of the theater mortified, and now I buy my sodas AT the theater.");
+            sb.Append("True story...");
+
+
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ stringFun/stringFun/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// escape character \ ignores the character to its right
+            //// new line \n
+            //// a tab \t
+            //string name = "Alexis";
+            //string quote = "The man said, \"Hello\",\n Alexis.";
+            //// the @ says there are no escape sequences
+            //string filename = @"C:\Users\Alexis";
+
+            //bool trueOrFalse = name.Contains("s");
+
+            //int length = name.Length; // 6 (because Alexis has  letters)
+
+            //Console.WriteLine(length);
+            //Console.ReadLine();
+            
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Jesse");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}

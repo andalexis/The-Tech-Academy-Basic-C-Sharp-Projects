00d2bd24fa48de54193aa4b2e58381a53c0a07b6
Alexis <a.anderson4110@gmail.com>
2019-09-19 22:52:01 -0500

    drill

---
+++ exceptionDrill/exceptionDrill/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exceptionDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Drill
+            try
+            {
+                List<int> numberList = new List<int> { 2, 4, 6, 10, 100 };
+                Console.WriteLine("This is a list of numbers:");
+                foreach (int i in numberList)
+                {
+                    Console.WriteLine(i);
+                }
+
+                Console.WriteLine("\nType a number for me to divide all these numbers by: ");
+                int userNum = Convert.ToInt16(Console.ReadLine());
+
+                foreach (int num in numberList)
+                {
+                    Console.WriteLine(" {0}/{1}={2} ", num, userNum, (num / userNum));
+                }
+                Console.ReadLine();
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Oops, please don't try to divide by zero");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Oops that wasn't a number!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("You've now exited this try/catch block");
+            Console.ReadLine();
+            
+        }
+    }
+}

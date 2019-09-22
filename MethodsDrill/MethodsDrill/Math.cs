df48271f8f3ad8e48cd1d416d6d0f2eed1abe765
Alexis <a.anderson4110@gmail.com>
2019-09-22 16:02:17 -0500

    methodDrill

---
+++ MethodsDrill/MethodsDrill/Math.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsDrill
+{
+    class Math
+    {
+        public int square;
+        public int numdouble;
+        public bool iseven;
+
+        public void SquareNumber(int userNumber)
+        {
+            square = userNumber * userNumber;
+        }
+
+        public void DoubleNumber(int userNumber)
+        {
+            numdouble = userNumber * 2;
+        }
+
+        public void isEven(int userNumber)
+        {
+            if (userNumber % 2 == 0)
+            {
+                iseven = true;
+            }
+        }
+
+        public void Print()
+        {
+            Console.Write("The square of that number is " + square + "\n");
+            Console.WriteLine("Double of that number is " + numdouble);
+            Console.Write("Is this number even?: " + iseven);
+        }
+    }
+}
---
+++ MethodsDrill/MethodsDrill/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsDrill
+{
+    public class Program
+    {
+        //public int userNumber = 0;
+        //public int square;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            Math math = new Math();
+            math.SquareNumber(userNumber); // calling method
+            math.DoubleNumber(userNumber);
+            math.Print(); // calling method
+            
+            Console.ReadLine();
+
+
+        }
+
+
+    }
+}

5f7e6b53469d91bd70eaff7b102325472547fcd6
Alexis <a.anderson4110@gmail.com>
2019-09-26 12:00:34 -0500

    drill

---
+++ MethodDrill2/MethodDrill2/Numbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill2
+{
+    public class Numbers
+    {
+        public static void ModifyValue(int i, int j)
+        {
+            i = i * 2;
+            Console.WriteLine("Your first number was doubled to = {0}", i);
+            Console.WriteLine("Your second number stayed the same = {0}", j);
+            return;
+        }
+    }
+}
---
+++ MethodDrill2/MethodDrill2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number: ");
+            int value1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Type another number: ");
+            int value2 = Convert.ToInt16(Console.ReadLine());
+
+            // calling method by value
+            Console.WriteLine("Your values are " + value1 + " and " + value2);
+            // calling method by parameters
+            Numbers.ModifyValue(value1, value2);
+            Console.ReadLine();
+        }
+    }
+    
+}

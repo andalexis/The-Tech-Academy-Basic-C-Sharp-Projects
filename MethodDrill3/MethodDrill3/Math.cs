cb53d08a372bac55f771e099c809c6e606148853
Alexis <a.anderson4110@gmail.com>
2019-10-04 12:35:13 -0500

    drill

---
+++ MethodDrill3/MethodDrill3/Math.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill3
+{
+    public class Math
+    {
+        // Create a class. In that class, 
+        // create a method that will take in an integer, 
+        // do a math operation to it and
+        // then return the answer as an integer.
+
+        public int Add(int a)
+        {
+            int result = a + 10;
+            return result;
+        }
+
+        public decimal Add(decimal b)
+        {
+            decimal result = Decimal.ToInt32(b * 10);
+            return result;
+        }
+
+        public string Add(string c)
+        {
+            int len = c.Length;
+            int num = len + 4;
+            string result = num.ToString();
+            return result;
+        }
+    }
+}
---
+++ MethodDrill3/MethodDrill3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDrill3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Math ob = new Math();
+
+            Console.WriteLine("Type a whole number: ");
+            int math1 = ob.Add(Convert.ToInt16(Console.ReadLine()));
+
+            Console.WriteLine("Type a decimal number: ");
+            decimal math2 = ob.Add(Convert.ToDecimal(Console.ReadLine()));
+
+            Console.WriteLine("Type a word: ");
+            string math3 = ob.Add(Convert.ToString(Console.ReadLine()));
+            Console.WriteLine(math1);
+            Console.WriteLine(math2);
+            Console.WriteLine(math3);
+            Console.ReadLine();
+        }
+    }
+}

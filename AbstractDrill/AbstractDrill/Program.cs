f58b23c490b461a1a2fe0358da6b2e114a564d26
Alexis <a.anderson4110@gmail.com>
2019-10-08 21:17:37 -0500

    drill

---
+++ AbstractDrill/AbstractDrill/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractDrill
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Employee person = new Employee();
+            person.FirstName = "Sample";
+            person.LastName = "Student";
+            person.SayName();
+            
+        }
+    }
+}

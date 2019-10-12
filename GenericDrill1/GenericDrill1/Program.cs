8b2eceb8c93cc4f6854e0b6c4edfc495b1c29833
Alexis <a.anderson4110@gmail.com>
2019-10-12 14:31:10 -0500

    drill

---
+++ GenericDrill1/GenericDrill1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDrill1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee<string> Employee1 = new Employee<string>();
+            Employee1.Things = new List<string>() { "apple", "pencil", "candy" };
+
+            Employee<int> Employee2 = new Employee<int>();
+            Employee2.Things = new List<int>() { 2, 5, 1, 7 };
+
+            foreach (var Thing in Employee1.Things)
+            {
+                Console.WriteLine(Thing);
+            }
+            Console.ReadLine();
+
+            foreach (var Thing in Employee2.Things)
+            {
+                Console.WriteLine(Thing);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}

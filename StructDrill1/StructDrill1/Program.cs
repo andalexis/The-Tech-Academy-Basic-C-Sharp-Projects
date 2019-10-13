46c2e3f6ad87ccb3961f1c7fc09d31b75d16dbde
Alexis <a.anderson4110@gmail.com>
2019-10-12 20:28:37 -0500

    drill

---
+++ StructDrill1/StructDrill1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructDrill1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //create an object of data type Number and assign an amount to it.
+            Number number = new Number();
+            number.Amount = 4;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}

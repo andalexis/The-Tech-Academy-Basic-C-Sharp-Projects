bee669ad1988fc9c7c35f445b13e375717deed88
Alexis <a.anderson4110@gmail.com>
2019-10-12 19:51:41 -0500

    drill

---
+++ Enum1/Enum1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the current day of the week: ");
+            //Assign the value to a variable of that enum data type you just created.
+            string value = Console.ReadLine().ToUpper();
+            DaysOfTheWeek day;
+            try
+            {
+                day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), value);
+                Console.WriteLine("Cool, have a great day.");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Thats not a day of the week");
+                Console.ReadLine();
+            }
+        }
+
+
+
+        public enum DaysOfTheWeek
+        {
+            MONDAY,
+            TUESDAY,
+            WEDNESDAY,
+            THURSDAY,
+            FRIDAY,
+            SATURDAY,
+            SUNDAY
+        }
+    }
+
+}

51b6f8617de311b9785ae933114fde366cf11c9d
Alexis <a.anderson4110@gmail.com>
2019-10-14 11:05:14 -0500

    drill

---
+++ LogDrill/LogDrill/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LogDrill
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number: ");
+            string text = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Student\Documents\GitHub\Logging\LogDrill.txt", text);
+
+            using (StreamReader file = new StreamReader(@"C:\Users\Student\Documents\GitHub\Logging\LogDrill.txt"))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    Console.ReadLine();
+                }
+            }
+            
+        }
+    }
+}
--- TwentyOne/TwentyOne/Dealer.cs
+++ TwentyOne/TwentyOne/Dealer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TwentyOne
 {
@@ -15,7 +16,13 @@ namespace TwentyOne
         public void Deal(List<Card> Hand)
         {
             Hand.Add(Deck.Cards.First());
-            Console.WriteLine(Deck.Cards.First().ToString() + "\n");
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            Console.WriteLine(card);
+            // logging each time a card is dealt
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Student\Documents\GitHub\Logging\21Log.txt", true))
+            {
+                file.WriteLine(card);
+            }
             Deck.Cards.RemoveAt(0);
         }
     }

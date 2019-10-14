2ac941fd1b1b0788cc766cc839029820d95d5637
Alexis <a.anderson4110@gmail.com>
2019-10-14 12:32:26 -0500

    drill

---
+++ DateTimeDrill/DateTimeDrill/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DateTimeDrill
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine("Current time: " + currentTime);
+            
+            Console.WriteLine("\nType a number:  ");
+            int userHr = Convert.ToInt32(Console.ReadLine());
+            DateTime futureTime = currentTime.AddHours(userHr);
+            Console.WriteLine("In {0} hours it wil exactly be: ", userHr);
+            Console.Write(futureTime);
+            
+            Console.ReadLine();
+           
+
+            
+
+        }
+    }
+}

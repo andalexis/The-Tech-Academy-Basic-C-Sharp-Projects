c50629121a3639cbe9800a38e8bb8c7008f34ddf
andalexis <a.anderson4110@gmail.com>
2020-01-22 19:09:22 -0700

    edit

--- Drill_finaltest/Drill_finaltest/Scripts/myViewModel.js
+++ Drill_finaltest/Drill_finaltest/Scripts/myViewModel.js
@@ -1,13 +1,25 @@
 ﻿
-function myViewModel() {
+//function myViewModel() {
    
-    this.firstWord = ko.observable('hello'),
-        this.secondWord = ko.observable('world'),
+//    this.firstWord = ko.observable('hello'),
+//        this.secondWord = ko.observable('world'),
 
-        this.fullSentence = ko.computed(function () {
-            return this.firstWord() + " " + this.secondWord();
-        }, this);    
-}
+//        this.fullSentence = ko.computed(function () {
+//            return this.firstWord() + " " + this.secondWord();
+//        }, this);    
+//}
 
+// Here's my data model
+var myViewModel = function (first, second) {
+    this.firstWord = ko.observable(first);
+    this.secondWord = ko.observable(second);
+
+    this.fullSentence = ko.pureComputed(function () {
+        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
+        return this.firstWord() + " " + this.secondWord();
+    }, this);
+};
+
+ko.applyBindings(new myViewModel("Hello", "World")); // This makes Knockout get to work
 
 
